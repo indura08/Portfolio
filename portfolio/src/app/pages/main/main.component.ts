3cc64d14e551495acef33042e59dcb5356842d08
indura08 <induraperera3@gmail.com>
2024-09-20 11:40:20 +0530

    new chnages to main page

--- portfolio/src/app/pages/main/main.component.ts
+++ portfolio/src/app/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
-  selector: 'app-main',
+  selector: 'main',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
